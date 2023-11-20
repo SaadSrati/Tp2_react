e11bd93a47670075ad9d8eee2ccc44fb25a392d1
saad srati <saadsrati2004@gmail.com>
2023-11-20 21:34:11 +0100

    Merge remote changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 <<<<<<< HEAD
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 =======
 import './index.css';
 import App from './App';
@@ -15,8 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-
-<<<<<<< HEAD
-reportWebVitals();
-=======
->>>>>>> 34747668e65fd21de626a481190e86979ff7984b
