851a871237a81a9a1d886c75e63d43257bb87233
saad srati <saadsrati2004@gmail.com>
2023-11-26 21:12:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import { TodoWrapper } from './Compenents/TodoWrapper';
 
@@ -11,16 +10,4 @@ function App() {
 }
 
 export default App;
-=======
-import Slider from "./Compenent/Slider";
 
-function App() {
-
-
-    return (
-      <div><Slider /></div>
-    )
-}
-
-export default App;
->>>>>>> 34747668e65fd21de626a481190e86979ff7984b
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './App';
-=======
+
 import './index.css';
-import App from './App';
->>>>>>> 34747668e65fd21de626a481190e86979ff7984b
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
